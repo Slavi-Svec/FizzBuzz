8e86dfd0e61da85a0cda17888c5bf65a754b2ca7
Slavi-Svec <slavi.svec@gmail.com>
2019-09-03 09:37:46 +1000

    add timesTwo method

---
+++ array_methods.js
@@ -0,0 +1,12 @@
+// // sort out numbers array from lowest to highest
+const numbers = [32323232,  2, 1, 4, 22, 565, 4, 12323223, 12, 34343434343434343434, 77777, 11111];
+const sortedNumbers = numbers
+    .sort((a, b) => a - b)
+    .map(a => Number(a));
+
+const timesTwo = numbers.map((num) => {
+    return num * 2;
+})
+
+
+
--- sort.js
+++
@@ -1,7 +0,0 @@
-// // sort out numbers array from lowest to highest
-const numbers = ['32323232',  '2', 1, 4, 22, 565, '4', '12323223', 12, '34343434343434343434', 77777, 11111];
-const sortedNumbers = numbers
-    .sort((a, b) => a - b)
-    .map(a => Number(a));
-
-console.log(sortedNumbers);
